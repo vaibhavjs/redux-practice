f73a6b3a95735c7de6d27ab08288f27aa036c75f
Vaibhav <vaibhavg1201@gmail.com>
2022-06-01 15:45:04 +0530

    commit

---
+++ redux-todo-list/src/redux/todoSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const todoSlice = createSlice({
+  name: "todos",
+  initialState: [
+    { id: 1, title: "todo1", completed: false },
+    { id: 2, title: "todo2", completed: false },
+    { id: 3, title: "todo3", completed: true },
+  ],
+  reducers: {
+    addTodo: (state, action) => {
+      const newTodo = {
+        id: Date.now(), //Nice way to get unique ID
+        title: action.payload.title,
+        completed: false,
+      };
+      state.push(newTodo);
+    },
+  },
+});
